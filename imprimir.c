15b57bece2d4ad226facfedd6786b041904c5210
smaring2 <sebasmaringallego@hotmail.com>
2024-02-17 13:37:00 -0500

    POC mensaje de error en consola

---
+++ imprimir.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    // Imprime un mensaje de error en stderr
+    fprintf(stderr, "¡Esto es un mensaje de error!\n");
+    return 1; // Devuelve un código de error
+}
